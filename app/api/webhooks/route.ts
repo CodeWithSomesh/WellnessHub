9c7d7533ee843e565421010493780b0503ef039e
CodeWithSomesh <codewithsomesh@gmail.com>
2025-06-08 23:45:18 +0800

    Fix naming issue in route.ts

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest) {
       const { id, email_addresses, first_name, last_name } = evt.data;
   
       const user = {
-        clerkId: id,
+        clerkID: id,
         email: email_addresses[0].email_address,
         firstName: first_name,
         lastName: last_name,
