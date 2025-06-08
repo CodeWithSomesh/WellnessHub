a669f6a19818589d17bf181141043933fde2fdab
CodeWithSomesh <codewithsomesh@gmail.com>
2025-06-08 22:06:47 +0800

    Added authentication successfully

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="w-full  border-b border-primary/20 sticky top-0 z-50">
+    <nav className="w-full  border-b border-primary/20 sticky top-0 z-100">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
