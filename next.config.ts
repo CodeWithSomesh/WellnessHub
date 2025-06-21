dc54e8d792e0cb22ad2f4d5186485fbfb85ad8f2
CodeWithSomesh <codewithsomesh@gmail.com>
2025-06-21 16:31:18 +0800

    Started the hero section of Fav Page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'img.buzzfeed.com',
+      'v1.exercisedb.io',
+      'maps.googleapis.com',
+      'maps.gstatic.com',
+      'maps.googleapis.com',
+      'lh3.googleusercontent.com',
+      'v2.exercisedb.io',
+      'exercisedb.p.rapidapi.com',
+      // Add other domains as needed
+    ],
+  },
 };
 
 export default nextConfig;
